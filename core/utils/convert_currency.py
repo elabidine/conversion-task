30e79dfa33515705a56c5bcd1a82d82fd772b898
elabidine <elabidine.zinou@gmail.com>
2024-12-14 00:30:44 +0100

    fix-bug

--- core/utils/convert_currency.py
+++ core/utils/convert_currency.py
@@ -14,7 +14,8 @@ from utils.print_object import _print_object
 
         
 # Not yet implemented
-def convert_currency(price:Decimal, from_currency: Currency, to_currency: Currency = None) -> Decimal:
+def convert_currency(price:Decimal, from_currency: Currency, to_currency: Currency = None,date=None) -> Decimal:
+    
     """ - Convert a price (price or amount) from one currency to another. 
     - Args:
         - price: The price to convert.
@@ -25,14 +26,15 @@ def convert_currency(price:Decimal, from_currency: Currency, to_currency: Curren
         - The converted price if both currencies are different
         - The original price if either currency is None, or if they are the same.
     - Raises:
-            - ValidationError: If the currencies are the same, or if either currency is None.
-            - ValidationError: If either provided currency is invalid.
+        - ValidationError: If the source currency is not provided.
+        - ValidationError: If either provided currency is invalid.
     """
     if not from_currency:
         raise ValidationError(_('The source currency must be provided.'))
     
     if not to_currency or from_currency == to_currency:
         return price
+    
     if not isinstance(from_currency, Currency) or not isinstance(to_currency, Currency):
         raise ValidationError(_('One or both provided currencies are invalid.'))
     
@@ -47,6 +49,7 @@ def convert_currency(price:Decimal, from_currency: Currency, to_currency: Curren
             return converted_value
 
 def get_exchange_rate(currency: Currency) -> Decimal:
+    
     """ - Retrieve the exchange rate for a given currency. Fetch from the API if not in the database
     or if the stored rate is outdated.
 
@@ -76,7 +79,8 @@ def get_exchange_rate(currency: Currency) -> Decimal:
     ExchangeRate.objects.update_or_create(
         currency=currency,
         defaults={
-        'rate': Decimal(rate),  # These are the fields to update or create
+        'rate': Decimal(rate),
+        'datetime':timezone.now  # These are the fields to update or create
     }
     )
     return Decimal(rate).quantize(Decimal('0.000001'),rounding=ROUND_HALF_UP)
@@ -96,29 +100,33 @@ def fetch_specific_rate_from_api(currency: Currency) -> Decimal:
     """
     API_URL_ALPHA = "https://www.alphavantage.co/query" 
     BASE_CURRENCY = "EUR"
-    API_KEY = "0O8QML1RVJW8JVXV"
+    API_KEY_ALPHA = "0O8QML1RVJW8JVXV"
     FUNCTION="CURRENCY_EXCHANGE_RATE"
 
     try:
-        response = requests.get(API_URL_ALPHA, params={"function":FUNCTION,"apikey": API_KEY,"from_currency": BASE_CURRENCY, "to_currency": currency.value})
+        response = requests.get(API_URL_ALPHA, params={"function":FUNCTION,"apikey": API_KEY_ALPHA,"from_currency": currency.value, "to_currency": BASE_CURRENCY})
         response.raise_for_status()  # Raise an error for HTTP errors
         
         data = response.json()
         
         rate = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
-          # Convert API date to timezone-aware datetime
-
-        return Decimal(rate).quantize(Decimal('0.000001'),rounding=ROUND_HALF_UP)
-    except (requests.RequestException, KeyError) as e:
-        _print_object(f"Failed to fetch rate for {currency}: {e}")
         
-    API_URL_FIXER = "http://data.fixer.io/api/latest"  # Replace with your API endpoint
-    API_KEY = "5736444ca2ffca70f6c6c17b39fab97a"
+        return Decimal(rate).quantize(Decimal('0.000001'),rounding=ROUND_HALF_UP)
+    
+    except (requests.RequestException, KeyError) as e:
+        _print_object(f"Failed to fetch rate for {currency}: {e} with ALPHA API")
+ 
+    API_URL_TWELVE = f"https://api.twelvedata.com/exchange_rate"
+    API_KEY_TWELVE="b2246d7458cd463b819b23449c357377"
+    
     try:
-        response = requests.get(API_URL_FIXER, params={"access_key": API_KEY,"base": BASE_CURRENCY, "symbols": currency.value})
+        response = requests.get(API_URL_TWELVE, params={"apikey":API_KEY_TWELVE,"symbol":f"{currency.value}/{BASE_CURRENCY}"})
+        
         response.raise_for_status()  # Raise an error for HTTP errors
         data = response.json()
-        rate = data["rates"][currency]
-        return Decimal(rate).quantize(Decimal('0.000001'),rounding=ROUND_HALF_UP)
+        
+        if "rate" in data:
+            return Decimal(data["rate"]).quantize(Decimal('0.000001'),rounding=ROUND_HALF_UP)
+        
     except (requests.RequestException, KeyError) as e:
-        raise RuntimeError(f"Failed to fetch rate for {currency}: {e}")
+            raise RuntimeError(f"Failed to fetch rate for {currency}: {e} with TWELVE API")
